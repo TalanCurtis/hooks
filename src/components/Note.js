067f6227f8d332a5a7d7e8ba6968053164b88e75
Alan Curtis <acurtis@badgersport.local>
2019-12-31 21:55:18 -0700

    custom react hook

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useContext } from 'react';
 import NotesContext from '../context/notes-context';
+import useMousePosition from '../hooks/useMousePosition';
+
 
 const Note = ({note}) => {
 
   const {dispatch} = useContext(NotesContext);
+  const position = useMousePosition();
 
   useEffect(()=>{
     console.log('Setting up effect');
@@ -16,6 +19,7 @@ const Note = ({note}) => {
        <div >
          <h3>{note.title}</h3>
          <h4>{note.body}</h4>
+         <p>{position.x}, {position.y} </p>
          <button onClick={()=>dispatch({type:'REMOVE_NOTE', title: note.title})}>X</button>
        </div>
   );
